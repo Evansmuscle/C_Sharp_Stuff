2053bbd948ea065ad13657bf1ab7ebd4d59f91a3
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-06-20 12:15:15 +0300

    feat: Push before format

---
+++ Chapter1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Chapter1
+{
+    class Program
+    {
+        void Types()
+        {
+            int x;
+            x = 10;
+
+            int y = 20;
+            int z = x + y;
+
+            double height = 300.5;
+            bool working = true;
+            char symbol = '@';
+            string name = "Khan";
+
+            const double PI = 3.14159;
+
+            // Type casting
+            double a = 3.501;
+            Int16 b = Convert.ToInt16(a);
+
+            Console.WriteLine(b);
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string? name = Console.ReadLine();
+
+            if (name != null)
+            {
+                Console.WriteLine("Your name is: " + name);
+            }
+        }
+    }
+}
